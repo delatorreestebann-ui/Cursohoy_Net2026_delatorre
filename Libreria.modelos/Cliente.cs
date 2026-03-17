09080953264bc5ca14d32a81d51ee5e82efb6f00
DelaTorre <Usuario@DESKTOP-C5I3SQH>
2026-03-17 09:11:44 -0500

    cambios de modelos Cliente y Paises

---
+++ Libreria.modelos/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Libreria.modelos
+{
+    internal class Cliente
+    {
+        [Key] public int Id { get; set; }
+
+        public string Nombre_Cliente { get; set; }
+        public string Apellido_Cliente { get; set; }
+        public string Correo_Cliente { get; set; }
+
+        public string Nombre_Usiario { get; set; }
+        public string contra_Cliente { get; set; }
+    }
+}
---
+++ Libreria.modelos/Paises.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Libreria.modelos
+{
+    public class Paises
+    {
+        [Key] public int Id { get; set; }
+        public string Nombre_Pais { get; set; }
+    }
+}
